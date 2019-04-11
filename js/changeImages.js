48bdc622241dc744fc37515e4990907951df0c11
yuyzer0 <49357298+yuyzer0@users.noreply.github.com>
2019-04-11 22:38:42 +0900

    Update changeImages.js

--- js/changeImages.js
+++ js/changeImages.js
@@ -1,45 +1,46 @@
 <script type="text/javascript">
   var picSrc = new Array();
+  var picJpg = ".jpg";
 
   for (var i = 1; i < 15; i++) {
     var num = (000 + i).slice( -4 );
-    var filePass = "images/annkw" + num;
+    var filePass = "images/annkw" + num + picJpg;
     picSrc.add(filePass);
   }
 
   for (var i = 1; i < 2; i++) {
     var num = (000 + i).slice( -4 );
-    var filePass = "images/book" + num;
+    var filePass = "images/book" + num + picJpg;
     picSrc.add(filePass);
   }
 
   for (var i = 1; i < 10; i++) {
     var num = (000 + i).slice( -4 );
-    var filePass = "images/hinata" + num;
+    var filePass = "images/hinata" + num + picJpg;
     picSrc.add(filePass);
   }
 
   for (var i = 1; i < 4; i++) {
     var num = (000 + i).slice( -4 );
-    var filePass = "images/keyaki" + num;
+    var filePass = "images/keyaki" + num + picJpg;
     picSrc.add(filePass);
   }
 
   for (var i = 1; i < 2; i++) {
     var num = (000 + i).slice( -4 );
-    var filePass = "images/non" + num;
+    var filePass = "images/non" + num + picJpg;
     picSrc.add(filePass);
   }
 
   for (var i = 1; i < 4; i++) {
     var num = (000 + i).slice( -4 );
-    var filePass = "images/other" + num;
+    var filePass = "images/other" + num + picJpg;
     picSrc.add(filePass);
   }
 
   for (var i = 1; i < 2; i++) {
     var num = (000 + i).slice( -4 );
-    var filePass = "images/travel" + num;
+    var filePass = "images/travel" + num + picJpg;
     picSrc.add(filePass);
   }
 
